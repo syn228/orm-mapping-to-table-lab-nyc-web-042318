1567154361a43a84db9aa5bde2bd8fedaa735b36
Aaron Na <syn228@nyu.edu>
2018-05-01 17:40:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,44 @@
 class Student
 
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+  
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+      );
+      SQL
+      DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE songs
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, grade)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")
+  end
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
